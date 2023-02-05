012700ff916aa44613601f78339af763111e97bf
richpignft <98395218+natowgka@users.noreply.github.com>
2023-02-05 14:01:00 +0800

    Add files via upload

---
+++ ibaidu-c.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?70b65f42e837653f1f3a35d1dd7de684";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
